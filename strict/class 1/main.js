5fe3ce66707b5084e60cc95fe519156828ab0eba
PulakBala <pulakbala0524@gmail.com>
2022-04-07 23:11:00 +0600

    six commit

---
+++ strict/class 1/main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var name = 'Pulak Bala';
+var age = 10;
+
+var address = 'Madaripur';
+
+console.log('My name is ' + name + 'and I am ' + age + ' years old ' );
+
+function aFunc() {
+    job : 'Student';
+    console.log('A Function with Strict Mode Enabled');
+}
+
+aFunc();
+
+console.log('I am a' + job + ' And I am from' + address);
